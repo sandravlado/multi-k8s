4e9d5299a64996d5e415b35013923668bb161306
Vlad <vladimir.bundalo@hotmail.com>
2022-08-22 09:59:34 +0100

    Updating docker push images

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t sandravlado/multi-client-k8s:latest -t sandravlado/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t sandravlado/multi-server-k8s-pgfix:latest -t sandravlado/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t sandravlado/multi-worker-k8s:latest -t sandravlado/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t stephengrider/multi-client-k8s:latest -t vbundalo/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t stephengrider/multi-server-k8s-pgfix:latest -t vbundalo/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t stephengrider/multi-worker-k8s:latest -t vbundalo/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push sandravlado/multi-client-k8s:latest
-docker push sandravlado/multi-server-k8s-pgfix:latest
-docker push sandravlado/multi-worker-k8s:latest
+docker push vbundalo/multi-client-k8s:latest
+docker push vbundalo/multi-server-k8s-pgfix:latest
+docker push vbundalo/multi-worker-k8s:latest
 
-docker push sandravlado/multi-client-k8s:$SHA
-docker push sandravlado/multi-server-k8s-pgfix:$SHA
-docker push sandravlado/multi-worker-k8s:$SHA
+docker push vbundalo/multi-client-k8s:$SHA
+docker push vbundalo/multi-server-k8s-pgfix:$SHA
+docker push vbundalo/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=sandravlado/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=sandravlado/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=sandravlado/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=vbundalo/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=vbundalo/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=vbundalo/multi-worker-k8s:$SHA
